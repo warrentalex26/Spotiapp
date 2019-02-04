608d762b8a10b47074290083de7cb163ae6c72c6
Alex Talavera <warrentalex26@hotmail.com>
2019-02-03 19:04:27 -0600

    Finalizado app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-// Importamos para poder hacer peticiones HTTP, los que terminan en Module van exportados en los imports mas abajo
+import { RouterModule } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+// Importar rutas
+import { ROUTES } from './app.routes';
+
+// Pipes
+import { NoimagePipe } from './pipes/noimage.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+
+
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+
 
 @NgModule({
   declarations: [
@@ -18,12 +29,16 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     HomeComponent,
     SearchComponent,
     ArtistaComponent,
-    NavbarComponent
+    NavbarComponent,
+    NoimagePipe,
+    DomseguroPipe,
+    TarjetasComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    RouterModule.forRoot( ROUTES, { useHash: true } )
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,24 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: []
 })
-export class HomeComponent implements OnInit {
-  paises: any = [];
-  // Al inyectarlo desde el constructor podemos acceder a todas las propiedades y metoodos de HttpClient
-  constructor(private http: HttpClient) {
-    console.log('Contructor del home llamado');
-    this.http.get('https://restcountries.eu/rest/v2/lang/es') // Con el subscribe obtenemos la informacion
-      .subscribe((data: any) => {
-        this.paises = data;
-        console.log(this.paises);
-      });
+export class HomeComponent {
+
+  nuevasCanciones: any[] = [];
+  loading: boolean;
+
+  error: boolean;
+  mensajeError: string;
+
+  constructor( private spotify: SpotifyService ) {
+
+    this.loading = true;
+    this.error = false;
+
+    this.spotify.getNewReleases()
+        .subscribe( (data: any) => {
+          this.nuevasCanciones = data;
+          this.loading = false;
+        }, ( errorServicio ) => {
+
+          this.loading = false;
+          this.error = true;
+          console.log(errorServicio);
+          this.mensajeError = errorServicio.error.error.message;
+
+        });
+
   }
 
-  ngOnInit() {
-  }
+
 
 }
